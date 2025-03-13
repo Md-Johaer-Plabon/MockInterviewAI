dc84c9163960b38cc094b29760d11f602599e296
Plabon <mdplabonda@gmail.com>
2025-03-14 02:53:15 +0600

    Update

---
+++ MockInterviewAI/Service/TextToSpeechService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockInterviewAI.Service
+{
+    internal class TextToSpeechService
+    {
+    }
+}
